c8e5cdc82c01c7839fbd8cd62d9f400c131f5203
Paskalis Agato <paskalis-agato@users.noreply.github.com>
2026-03-21 08:46:45 +0700

    fix: rename inventory to app-inventory for cache-busting

--- apps/inventory/vite.config.ts
+++ apps/inventory/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/inventory/',
+  base: '/app-inventory/',
   plugins: [react()],
   resolve: {
     alias: {
